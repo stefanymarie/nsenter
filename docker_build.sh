5c8ceff018add45f1da89fe058a94aa04d7b5880
Stefany Serino <stefanyserino@gmail.com>
2015-02-17 19:30:46 +0800

    tag with build number

--- docker_build.sh
+++ docker_build.sh
@@ -6,5 +6,5 @@ git checkout $VERSION
 VERSION_NAME=${VERSION#release\/}
 
 sudo docker build -t musashi/nsenter:$VERSION_NAME .
-sudo docker tag musashi/nsenter:$VERSION_NAME $DOCKER_BASE_URL/musashi/nsenter:$VERSION_NAME
-sudo docker push $DOCKER_BASE_URL/musashi/nsenter:$VERSION_NAME
+sudo docker tag musashi/nsenter:$VERSION_NAME $DOCKER_BASE_URL/musashi/nsenter:${VERSION_NAME}_${BUILD_NUMBER}
+sudo docker push $DOCKER_BASE_URL/musashi/nsenter:${VERSION_NAME}_${BUILD_NUMBER}
